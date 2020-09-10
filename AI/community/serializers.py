b164d045aec819812b44387664d3dcfb24a99e3e
limjunseong <gbhjk46@gmail.com>
2020-09-10 15:52:57 +0900

    S03P22D107-1 | notice, suggestion, comment

--- AI/community/serializers.py
+++ AI/community/serializers.py
@@ -9,10 +9,31 @@ class NoticeListSerializer(serializers.ModelSerializer):
         fields = ('id', 'title','content','user',)
 
 
-
 class NoticeSerializer(serializers.ModelSerializer):
     user = UserSerializer(required=False)
     class Meta:
         model = Notice
         fields = '__all__'
         read_only_fields = ('id', 'user', 'created_at',)
+
+
+class SuggestionListSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    class Meta:
+        model = Suggestion
+        fields = '__all__'
+
+
+class SuggestionSerializer(serializers.ModelSerializer):
+    user = UserSerializer(required=False)
+    comments = CommentSerializer(many=True, read_only=True)
+    class Meta:
+        model = Suggestion
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = UserSerializer(required=False)
+    class Meta:
+        model = Comment
+        fields = '__all__'
