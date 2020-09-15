f10b67ac3db37b993148cb2a686ef530fc8ef963
안시경 <jeongseok95@naver.com>
2020-09-15 18:11:24 +0900

    pull

--- newlearn/src/App.vue
+++ newlearn/src/App.vue
@@ -1,23 +1,26 @@
 <template>
   <v-app>
     <Header />
-      <v-spacer></v-spacer>
+      <!-- <v-spacer></v-spacer> -->
     <v-main>
-      <Signin />
+      <!-- <Signin /> --> 
+      <Myinfo />
     </v-main>
   </v-app>
 </template>
 
 <script scoped>
-import Header from './components/Header.vue';
-import Signin from './views/Signin.vue'
+import Header from './components/Header.vue'
+// import Signin from './views/accounts/Signin.vue'
+import Myinfo from './views/accounts/Myinfo'
 
 export default {
   name: 'App',
 
   components: {
     Header,
-    Signin,
+    // Signin,
+    Myinfo,
 },
 
   data: () => ({
