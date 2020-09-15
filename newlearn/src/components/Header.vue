3a83f7e0d37fd830837343fcb8938d4c4599e0b8
Lunarus00 <moongur1900@naver.com>
2020-09-15 17:14:34 +0900

    S03P22D107-39 | header 출석부와 router 연결 설정

--- newlearn/src/components/Header.vue
+++ newlearn/src/components/Header.vue
@@ -21,7 +21,7 @@
                             </v-list-item-content>
                         </v-list-item>
                         <v-list v-if="dropDrawer">
-                            <v-list-item style="margin-left:20px;">
+                            <v-list-item @click="goAttendance" style="margin-left:20px;">
                                 <v-list-item-content>
                                     <v-list-item-title>출석부</v-list-item-title>
                                 </v-list-item-content>
@@ -31,7 +31,7 @@
                                     <v-list-item-title>내 정보 수정</v-list-item-title>
                                 </v-list-item-content>
                             </v-list-item>
-                            <v-list-item link @click="goPointList" style="margin-left:20px;">
+                            <v-list-item @click="goPointList" style="margin-left:20px;">
                                 <v-list-item-content>
                                     <v-list-item-title>포인트 조회</v-list-item-title>
                                 </v-list-item-content>
@@ -106,6 +106,9 @@ export default {
     methods: {
         goPointList() {
             this.$router.push('/mypage/pointList')
+        },
+        goAttendance() {
+            this.$router.push('/mypage/attendance')
         }
     }
 }
