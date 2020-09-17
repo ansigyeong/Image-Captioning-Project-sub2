8af5dfa52df577547fdc099ec799633e8c149467
Lunarus00 <moongur1900@naver.com>
2020-09-17 15:59:07 +0900

    app.vue에 header 표시, home에서 삭제

--- newlearn/src/App.vue
+++ newlearn/src/App.vue
@@ -1,25 +1,30 @@
 <template>
-  <v-app>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link v-if="!isLoggedIn" :to="{ name: 'Login' }">Login</router-link> | 
-      <router-link v-if="!isLoggedIn" :to="{ name: 'Signup' }">Signup</router-link>
-      <router-link v-if="isLoggedIn" :to="{ name: 'Logout' }">Logout</router-link>
-    </div>
-    <router-view />
+  <v-app id="app">
+    <Header/>
+    <main>
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link v-if="!isLoggedIn" :to="{ name: 'Login' }">Login</router-link> | 
+        <router-link v-if="!isLoggedIn" :to="{ name: 'Signup' }">Signup</router-link>
+        <router-link v-if="isLoggedIn" :to="{ name: 'Logout' }">Logout</router-link>
+      </div>
+      <router-view />
+    </main>
   </v-app>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   computed: {
     ...mapGetters(['isLoggedIn'])
   },
-  componenets : [
-  ],
+  components : {
+    Header,
+  },
   methods: {
   },
   
--- newlearn/src/views/Home.vue
+++ newlearn/src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <Header></Header>
     <h1>Home Page</h1>
   </div>
 </template>
@@ -8,12 +7,10 @@
 <script>
 // @ is an alias to /src
 
-import Header from '../components/Header.vue'
 
 export default {
   name: 'Home',
   components: {
-    Header,
   }
 }
 </script>
